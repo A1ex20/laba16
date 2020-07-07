6cdaffc1b853a36eb31ee5a717add033624195c0
Sasha Girl <sd_ks@bk.ru>
2020-07-07 16:30:34 +0800

    Добавьте файлы проекта.

---
+++ laba16/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace laba16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ввести 5 элементов и Удалить 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество элементов в стеке";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Элементы в стеке";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Элементы в стеке";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Произвольное количество элементов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(27, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 190);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1 задание";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(329, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(461, 190);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2 задание";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "ок";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Элементы в стеке";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 246);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ laba16/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        //Задание 1
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            label3.Text = "";
+            //Создаем массив из 5 элементов
+            string[] newFive = new string[5] { "Первый", "Второй", "Третий", "Четвертый", "Пятый"};
+            //Создаем стек
+            Stack<string> NewStack = new Stack<string>();
+                for (int i = 0; i < 5; i++)
+                {
+                //добавляем в стек эл-ты из массива
+                    NewStack.Push(newFive[i]);                    
+                }
+                foreach (var vivvodEl in NewStack) label2.Text += vivvodEl + " ";
+                //удаляем из стека два эл-та
+                for (int i = 0; i < 2; i++)
+                { NewStack.Pop(); }
+                foreach (var vivvodEl in NewStack) label3.Text += vivvodEl + " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            label3.Text = "";
+            int kolvo = Convert.ToInt32(textBox1.Text);
+            Stack<int> NewStack = new Stack<int>();
+            for (int i = 0; i < kolvo; i++)
+            {
+                NewStack.Push(i);
+            }
+            foreach (var vivvodEl in NewStack) label2.Text += vivvodEl + " ";
+            for (int i = 0; i < 2; i++)
+            { NewStack.Pop(); }
+            foreach (var vivvodEl in NewStack) label3.Text += vivvodEl + " ";
+
+        }
+        //Задание 2
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "Эл-ты в обратном порядке: ";
+            string[] Mas = new string[10] {"один",
+                "два", 
+                "три", 
+                "четыре",
+                "пять", 
+                "шесть", 
+                "семь", 
+                "восемь", 
+                "девять", 
+                "десять" };
+            Queue<string> NewQue = new Queue<string>();
+            //добавление 10 элементов в конец очереди
+            for (int i = 0; i < 10; i++)
+            { NewQue.Enqueue(Mas[i]); }
+            foreach (var viv in NewQue) label6.Text += viv + " ";
+            label6.Text += "\nЭ-ты которые остались: ";
+            //извлечение 5 элементов
+            for (int i = 0; i < 5; i++)
+            { NewQue.Dequeue(); }
+            foreach (var viv in NewQue) label6.Text += viv + " ";
+            //просмотр первого эл-та
+            label6.Text += "\nПервый эл-т: ";
+            label6.Text += NewQue.Peek();
+            //Удаляем все эл-ты
+           NewQue.Clear();
+            label6.Text += "\nОчистка!";
+            foreach (var viv in NewQue) label6.Text += viv + " ";
+            //Проверяем пуст ли стак
+            if (NewQue.Count() > 0)
+            {
+
+                label6.Text += "\nСтак не пуст"; 
+            }
+            else
+            {
+                label6.Text += "\nСтак пуст";
+            }
+
+        }
+    }
+}
